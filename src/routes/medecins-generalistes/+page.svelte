eb44d7b926d075c3b85a3d80b40cb11d02e69698
zinoka <alex.abruzzese96@gmail.com>
2024-11-22 13:23:17 +0100

    fix footer

--- src/routes/medecins-generalistes/+page.svelte
+++ src/routes/medecins-generalistes/+page.svelte
@@ -192,7 +192,7 @@
             <div style="height: 10vh"></div>
         </div>
         <div class="footer-text">
-            <p style="margin-top: 1vh; margin-bottom: 1.5vh;">© 2024 Copyright Maison de Santé Pluriprofessionnelle du Hameau</p>
+            <p style="margin-top: 1vh; margin-bottom: 10px;">© 2024 Copyright Maison de Santé Pluriprofessionnelle du Hameau</p>
             <p style="margin-bottom: 1vh;">Créé par Zino-Tech</p>
         </div>
     </body>
