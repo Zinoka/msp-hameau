859595503c7fc035793098b3b276ba3211504747
Zinoka <alex.abruzzese96@gmail.com>
2024-11-25 10:05:02 +0100

    remove console log

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,6 @@
     let isburgerMenuOpen = false;
     let init = true;
 
-    console.log(data)
-
     function goTo(url: string) {
         setTimeout(() => {
             isburgerMenuOpen = false;
@@ -37,7 +35,6 @@
     <head>
         <style>html{visibility: hidden;opacity:0;}</style>
         <title>MSP | Accueil</title>
-        <title>Page</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <link rel="preconnect" href="https://fonts.googleapis.com">
