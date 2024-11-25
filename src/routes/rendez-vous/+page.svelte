f25691ec8793138d2f1a730ac4edef09910e60d2
Zinoka <alex.abruzzese96@gmail.com>
2024-11-25 09:54:09 +0100

    phone

--- src/routes/rendez-vous/+page.svelte
+++ src/routes/rendez-vous/+page.svelte
@@ -134,7 +134,7 @@
             <div class="emergency">
                 <div class="emergency-title">En cas d'urgence</div>
                 <div class="emergency-text">
-                    <p>Le soir, la nuit et le week-end, vous pouvez appeler le <a href="tel:166177" style="color: white;">166177</a> pour joindre le médecin d'astreinte.</p>
+                    <p>Le soir, la nuit et le week-end, vous pouvez appeler le <a href="tel:116117" style="color: white;">116117</a> pour joindre le médecin d'astreinte.</p>
                     <p>En cas d'urgence en journée, la nuit ou le week-end, composez le <a href="tel:15" style="color: white;">15</a>.</p>
                 </div>
             </div>
