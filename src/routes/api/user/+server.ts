19ed08b367869f92ff780fedfb6e89e16c9e4473
zinoka <alex.abruzzese96@gmail.com>
2024-11-26 12:02:34 +0100

    fix

--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -1,6 +1,7 @@
 import { getUserByEmail } from "$lib/entities/User";
 
 export const POST = async (event) => {
+  console.log("POST /api/user/", event.request);
   const body = await event.request.json();
   const user = await getUserByEmail(body);
   return new Response(JSON.stringify(user));
