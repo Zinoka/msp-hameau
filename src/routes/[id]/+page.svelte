4d6f7be3c70f5d831f7c59dffab27483f62e978f
zinoka <alex.abruzzese96@gmail.com>
2024-11-27 09:07:25 +0100

    add style for article page

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,8 +1,109 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import type { PageServerData } from "./$types"
 
 	export let data: PageServerData
 
+	let isburgerMenuOpen = false;
+    let init = true;
+
+    function goTo(url: string) {
+        setTimeout(() => {
+            isburgerMenuOpen = false;
+            goto(url);
+        }, 0);
+    }
+
+    function burgerToggle() {
+        init = false;
+        isburgerMenuOpen = !isburgerMenuOpen;
+    }
+
+	async function scrollToDiv() {
+        await goto("/");
+        const element = document.getElementById("practitioners");
+        element?.scrollIntoView({ behavior: 'smooth' });
+    }
 </script>
 
-<span>{ data.article.title }</span>
+<html lang="fr">
+    <head>
+        <style>html{visibility: hidden;opacity:0;}</style>
+        <title>MSP | Article { data.article.id }</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="preconnect" href="css/index.css">
+        <link rel="preconnect" href="css/rdv.css">
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa" rel="stylesheet">
+        <link rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" href="css/rdv.css">
+
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="title">Maison de Santé Pluriprofessionnelle</div>
+            <div class="right-buttons">
+                <button class="navbar-button" on:click={() => goTo("/")}>Accueil</button>
+                <button class="navbar-button" on:click={() => scrollToDiv()}>Les Praticiens</button>
+                <button class="navbar-button" on:click={() => goTo("/contact")}>Contact</button>
+                <button class="navbar-button" on:click={() => goTo("/horaires")}>Horaires</button>
+                <button class="navbar-button" on:click={() => goTo("/rendez-vous")}>Rendez-vous</button>
+            </div>
+            {#if isburgerMenuOpen}
+                <button class="burger-menu" id="burgerMenu" on:click={() => burgerToggle()}>
+                    <div class="burger-menu-first-line-to-open"></div>
+                    <div class="burger-menu-second-line-to-open"></div>
+                    <div class="burger-menu-third-line-to-open"></div>
+                </button>
+            {:else if !isburgerMenuOpen && init}
+                <button class="burger-menu" id="burgerMenu" on:click={() => burgerToggle()}>
+                    <div class="burger-menu-first-line"></div>
+                    <div class="burger-menu-second-line"></div>
+                    <div class="burger-menu-third-line"></div>
+                </button>
+            {:else if !isburgerMenuOpen && !init}
+                <button class="burger-menu" id="burgerMenu" on:click={() => burgerToggle()}>
+                    <div class="burger-menu-first-line-to-close"></div>
+                    <div class="burger-menu-second-line-to-close"></div>
+                    <div class="burger-menu-third-line-to-close"></div>
+                </button>
+            {/if}
+        </div>
+        {#if isburgerMenuOpen}
+            <div class="burger-menu-panel">
+                <button class="burger-menu-button" on:click={() => goTo("/")}>Accueil</button>
+				<button class="burger-menu-button" on:click={() => scrollToDiv()}>Les Praticiens</button>
+                <button class="burger-menu-button" on:click={() => goTo("/contact")}>Contact</button>
+                <button class="burger-menu-button" on:click={() => goTo("/horaires")}>Horaires</button>
+                <button class="burger-menu-button" on:click={() => goTo("/rendez-vous")}>Rendez-vous</button>
+            </div>
+        {:else if !isburgerMenuOpen && !init}
+            <div class="burger-menu-panel-closing">
+                <button class="burger-menu-button" on:click={() => goTo("/")}>Accueil</button>
+                <button class="burger-menu-button" on:click={() => scrollToDiv()}>Les Praticiens</button>
+                <button class="burger-menu-button" on:click={() => goTo("/contact")}>Contact</button>
+                <button class="burger-menu-button" on:click={() => goTo("/horaires")}>Horaires</button>
+                <button class="burger-menu-button" on:click={() => goTo("/rendez-vous")}>Rendez-vous</button>
+            </div>
+        {/if}
+		<div class="welcome-image">
+			<img src="images/doctors.png" alt="docteurs msp hameau">
+		</div>
+		<div class="rdv-tuto">
+            <div class="rdv-tuto-title">{ data.article.title }</div>
+            <div class="rdv-tuto-sub-title">Publié le { data.article.created_at }</div>
+            <div class="rdv-tuto-text">
+                <div class="tuto-text">
+                    <p>{ data.article.description }</p>
+                </div>
+            </div>
+            <div style="height: 10vh"></div>
+        </div>        
+        <div class="footer-text">
+            <p style="margin-top: 1vh; margin-bottom: 10px;">© 2024 Copyright Maison de Santé Pluriprofessionnelle du Hameau</p>
+            <p style="margin-bottom: 1vh;">Créé par Zino-Tech</p>
+        </div>
+    </body>
+</html>
