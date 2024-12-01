1eb040a3183fc9e54c065b23907fa3d2237ee0cb
Zinoka <alex.abruzzese96@gmail.com>
2024-12-01 14:50:06 +0100

    update header

--- src/lib/stores/articleStore.ts
+++ src/lib/stores/articleStore.ts
@@ -15,6 +15,7 @@ export const createArticle = async (article: any) => {
   await fetch("/api/article/post/", {
     method: "POST",
     headers: {
+      "Content-Type": "application/json",
       "x-sveltekit-action": "true",
     },
     body: article,
