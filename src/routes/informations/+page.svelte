4189906ca86faa6bce7a814eb482cce6fc80c79a
zinoka <alex.abruzzese96@gmail.com>
2024-12-03 11:07:17 +0100

    update infos

--- src/routes/informations/+page.svelte
+++ src/routes/informations/+page.svelte
@@ -107,29 +107,16 @@
         {/if}
         <div class="contact-container">
             <div class="contact-infos">
-
-                <div class="contact-title">Nos horaires</div>
-                <div class="category">Médecins de la Maison de Santé</div>
-                <div class="contact-text" style="margin-bottom: 50px;">
-                    <div>
-                        <p>Les secrétaires vous accueillent pour la prise de rendez-vous ou pour tout renseignement du lundi au vendredi de 8h30 à 12h et de 14h à 18h, le samedi de 8h30 à 11h30.</p>
-                        <p>Consultations : sur rendez-vous du lundi au vendredi de 8h à 12h et de 14h à 18h30, et le samedi matin de 8h à 12h.</p>
-                        <p>Urgences : tous les matins de lundi à vendredi, accueil sans rendez-vous de 8h à 11h.</p>
-                    </div>
-                </div>
-        
-                
                 <div class="contact-title">Nous contacter</div>
-                <p class="contact-text">Contacter les médecins de la Maison de Santé</p>
-                <p class="contact-text">Le secrétariat médical est joignable au <strong><a href="tel:0559840002">05 59 84 00 02</a></strong> selon les modalités suivantes :</p>
-                <ul>
-                    <li>Lundi de <strong>8h30 à 12h00 et de 14h00 à 19H00</strong></li>
-                    <li>Mardi de <strong>8h30 à 12h00 et de 14h00 à 18h30</strong></li>
-                    <li>Mercredi de <strong>8h30 à 12h00</strong></li>
-                    <li>Jeudi de <strong>8h30 à 12h00</strong></li>
-                    <li>Vendredi de <strong>8h30 à 12h00 et 14h00 à 18h30</strong></li>
-                </ul>
-                <p class="contact-text" style="margin-top: 2vh;">Attention: Vous souhaitez contacter un praticien para-médical ?</p>
+                <div class="contact-text" style="margin-bottom: 50px;">
+                    <p>La Maison de Santé Pluriprofessionnelle est ouverte tous les jours de <strong>8h30 à 19h00</strong></p>
+                    <p style="margin-top: 30px; margin-bottom: 20px;">Accueil téléphonique au <strong><a href="tel:0559840002">05 59 84 00 02</a></strong></p>
+                    <ul>
+                        <li style="margin-bottom: 10px;">Du lundi au vendredi de <strong>8h30 à 19h00</strong></li>
+                        <li>Le samedi de <strong>8h00 à 12h00</strong> (sur rendez-vous pris le jour même)</li>
+                    </ul>
+                </div>
+                <p class="contact-text"><strong>Attention:</strong> Vous souhaitez contacter un praticien para-médical ?</p>
                 <p class="contact-text"><strong>Contactez-le directement.</strong></p>
             </div>
             <Map />
