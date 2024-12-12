3dd012d77254af3a983a207c996b2adc313319de
zinoka <alex.abruzzese96@gmail.com>
2024-12-12 12:04:26 +0100

    change rdv text

--- src/routes/rendez-vous/+page.svelte
+++ src/routes/rendez-vous/+page.svelte
@@ -113,11 +113,10 @@
             <div class="rdv-tuto-sub-title">Avec un médecin</div>
             <div class="rdv-tuto-text">
                 <div class="tuto-text">
-                    <p>Vous pouvez joindre le secrétariat de la maison de santé au <a href="tel:0559840002"><strong>05 59 84 00 02</strong></a> pendant les heures d'ouverture qui sont les suivantes :</p>
-                    <ul>
-                        <li>de <strong>8h30 à 19h00</strong> du lundi au vendredi</li>
-                        <li>de <strong>8h30 à 12h00</strong> le samedi (pas de prise de rendez-vous non urgent)</li>
-                    </ul>
+                    <p>Vous pouvez joindre les secrétaires médicales de <strong>8h30 à 18h00</strong> du lundi au vendredi au <a href="tel:0559840002"><strong>05 59 84 00 02</strong></a> pour les Dr Isabelle Ader Casedevant, Dr Fanny Le Guen, Dr Laure Elise Martin</p>
+                    <p>au <a href="tel:0559841227"><strong>05 59 84 12 27</strong></a> pour le Dr Pierre Casedevant</p>
+                    <p>au <a href="tel:0559026397"><strong>05 59 02 63 97</strong></a> pour le Dr Julie Canton Arino</p>
+                    <p>Une permanence téléphonique pour les urgences des patients de la maison de santé est assurée par les médecins de <strong>18h00 à 19h00</strong> du lundi au vendredi et de <strong>8h00 à 12h00</strong> le samedi (pas de prise de rendez-vous non urgent)</p>
                     <p>Vous pouvez aussi prendre rendez-vous sur Easydoct</p>
                     <button class="tuto-button" on:click={() => openExternalLink("https://www.easydoct.com/rdv/msp-du-hameau-pau")}>Prendre rendez-vous sur Easydoct</button>
                 </div>
