b622b5dcb31af9d14be715c834e27891a57c339b
zinoka <alex.abruzzese96@gmail.com>
2025-01-30 16:10:02 +0100

    add building

--- src/lib/server/database/database.ts
+++ src/lib/server/database/database.ts
@@ -1,3 +1,4 @@
+import { building } from '$app/environment';
 import { DataSource } from "typeorm";
 import { env_variables } from "../../../hooks.server";
 import { Article } from "$lib/entities/Article";
@@ -6,32 +7,34 @@ import { User } from "$lib/entities/User";
 class TypeOrm {
   private static instance: Promise<DataSource> | null = null;
 
-  private constructor() {}
+  private constructor() { }
 
   public static getDb(): Promise<DataSource> {
-    if (!TypeOrm.instance) {
-      TypeOrm.instance = new DataSource({
-        type: "mysql",
-        host: env_variables.parsed.DB_HOST || "localhost",
-        port: 3306,
-        username: env_variables.parsed.DB_USERNAME,
-        password: env_variables.parsed.DB_PASSWORD,
-        database: "msp_hameau",
-        synchronize: false,
-        entities: [Article, User],
-        logging: true
-      })
-        .initialize()
-        .then((fulfilled) => {
-          console.info("Database has been initialized !");
-          return fulfilled;
+    if (!building) {
+      if (!TypeOrm.instance) {
+        TypeOrm.instance = new DataSource({
+          type: "mysql",
+          host: env_variables.parsed.DB_HOST || "localhost",
+          port: 3306,
+          username: env_variables.parsed.DB_USERNAME,
+          password: env_variables.parsed.DB_PASSWORD,
+          database: "msp_hameau",
+          synchronize: false,
+          entities: [Article, User],
+          logging: true
         })
-        .catch((err) => {
-          console.error("Error during Data Source initialization", err);
-          return null;
-        });
+          .initialize()
+          .then((fulfilled) => {
+            console.info("Database has been initialized !");
+            return fulfilled;
+          })
+          .catch((err) => {
+            console.error("Error during Data Source initialization", err);
+            return null;
+          });
+      }
+      return TypeOrm.instance;
     }
-    return TypeOrm.instance;
   }
 }
 
