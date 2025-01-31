5a57017d2b2bb0c4daede9e0f751461dc67822cb
zinoka <alex.abruzzese96@gmail.com>
2025-01-31 10:33:18 +0100

    change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,11 +7,16 @@
 	import CookieBanner from "$lib/components/CookieBanner.svelte";
 	import CookieConsent from "$lib/components/CookieConsent.svelte";
 	import NavBar from "$lib/components/NavBar.svelte";
+	import Cookies from "js-cookie";
 
 	initializeStores();
 
 	try {
 		onMount(async () => {
+			const preferences = JSON.parse(
+				Cookies.get("cookie_preferences") || "{}",
+			);
+			console.log("pref", preferences);
 			const authUser: AuthUser | null | undefined = $userStore;
 			if (
 				(!authUser && $page.url.pathname === "/protected") ||
@@ -27,4 +32,5 @@
 </script>
 
 <NavBar />
+<CookieBanner />
 <slot />
