10fdc114d6099c602fbd0aa7b5bc798bb5f22384
zinoka <alex.abruzzese96@gmail.com>
2025-01-31 10:31:04 +0100

    move cookies banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,6 @@
 	import CookieBanner from "$lib/components/CookieBanner.svelte";
 	import CookieConsent from "$lib/components/CookieConsent.svelte";
 	import NavBar from "$lib/components/NavBar.svelte";
-	import Cookies from "js-cookie";
 
 	initializeStores();
 
@@ -21,11 +20,6 @@
 			) {
 				await goto("/");
 			}
-
-			const preferences = JSON.parse(
-				Cookies.get("cookie_preferences") || "{}",
-			);
-			console.log("pref", preferences);
 		});
 	} catch (err) {
 		throw new Error(`Erreur lors de l'affichage de la page "/"\n ${err}`);
@@ -33,5 +27,4 @@
 </script>
 
 <NavBar />
-<CookieBanner />
 <slot />
